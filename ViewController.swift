e801f1ab38b67c17c7281414e94a95d30c0db22a
PN <priteshrna@gmail.com>
2020-10-08 15:39:56 -0400

    text update

--- ViewController.swift
+++ ViewController.swift
@@ -42,7 +42,7 @@ class ViewController: UIViewController {
         tv.clipsToBounds = true
         tv.isEditable = false
         tv.font = .systemFont(ofSize: 21)
-        tv.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        tv.text = "Take a Photo of a To ID a Flower"
         
         return tv
     }()
